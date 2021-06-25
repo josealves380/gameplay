011ae350a0efea09ca6d7fa39e42c8c5855984a9
josealves380 <netopereira464@gmail.com>
2021-06-24 23:05:40 -0300

    Criando novos componentes e configurando svg que não é nativo do react-native

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
+import { categories } from '../../utils/categories';
+
+import { Category } from '../Category';
 
 import { styles } from './styles';
 
-export function CategorySelect() {
+type Props = {
+    categorySelected: string;
+    setCategory : (categoryId: string) => void;
+}
+
+export function CategorySelect({
+    categorySelected,
+    setCategory
+}: Props) {
     return (
         <ScrollView
          horizontal
@@ -11,7 +22,17 @@ export function CategorySelect() {
          showsHorizontalScrollIndicator={false}
          contentContainerStyle={{ paddingRight: 40}}
          >
-
+             {
+                 categories.map(category => (
+                     <Category
+                     key={category.id}
+                     title={category.title}
+                     icon={category.icon}
+                     checked={category.id === categorySelected} 
+                     onPress={() => setCategory(category.id)}
+                     />
+                 ))
+             }
         </ScrollView>
     )
 }
--- src/components/CategorySelect/styles.ts
+++ src/components/CategorySelect/styles.ts
@@ -3,8 +3,9 @@ import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    }
+        minHeight: 120,
+        maxHeight: 120,
+        paddingLeft: 24,
+    },
 
 });
